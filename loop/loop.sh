d3ea482e4d9d31cd1c2baccdfe29523b35963d06
Chitsanupong <first30952@gmail.com>
2021-09-27 11:45:17 +0700

    feat: add while loop

--- loop/loop.sh
+++ loop/loop.sh
@@ -7,10 +7,18 @@
 # done
 
 # LOOP TO RENAME FILES
-FILES=$(ls *.html)
-NEW="new"
-for FILE in $FILES
+# FILES=$(ls *.html)
+# NEW="new"
+# for FILE in $FILES
+#     do 
+#         echo "Renaming $FILE to new-$FILE"
+#         mv $FILE $NEW-$FILE
+# done
+
+# WHILE LOOP - READ THROUGH A FILE LINE BY LINE
+LINE=1
+while read -r CURRENT_LINE
     do 
-        echo "Renaming $FILE to new-$FILE"
-        mv $FILE $NEW-$FILE
-done
+        echo "$LINE: $CURRENT_LINE"
+        ((LINE++))
+done < "./story.txt"
